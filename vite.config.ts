20f614c54b88f62e72f1bd4eeb0ebc8bd77c1e54
Bitu-hane <bitanya456@gmail.com>
2026-08-21 10:44:03 +0300

    Fix vite config rollup entry point for production build

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig(() => {
       rollupOptions: {
         input: {
           main: path.resolve(__dirname, 'index.html'),
-          manifest: path.resolve(__dirname, 'manifest-ui/index.html'),
+        //  manifest: path.resolve(__dirname, 'manifest-ui/index.html'),
         },
       },
     },
